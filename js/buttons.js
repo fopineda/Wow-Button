1d193ef3c04e3e162da0e88cb1d59b817617df36
fopineda <fopineda95@gmail.com>
2016-07-13 11:25:40 -0400

    WowButtons-Web

---
+++ js/buttons.js
@@ -0,0 +1,11 @@
+/*
+<!--- Created by: Felipe O Pineda --->
+<!--- fopineda95@gmail.com --->
+<!--- Code away :) --->
+*/
+
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("You clicked me :)");
+  });
+});
